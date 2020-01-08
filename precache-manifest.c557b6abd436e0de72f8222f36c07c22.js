f18e30b974c3c0b4ec5670250e9cb81fbafc644b
Arun Nair <arunnair9319@gmail.com>
2020-01-08 14:28:21 +0530

    Updates

---
+++ precache-manifest.c557b6abd436e0de72f8222f36c07c22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0ef065a45f6aa7014d38df04e1dbd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbbdd3ce3d1b62445140",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "715c04caf7bf2ac11743",
+    "url": "/static/css/main.a8626ea2.chunk.css"
+  },
+  {
+    "revision": "dbbdd3ce3d1b62445140",
+    "url": "/static/js/2.04e4f5be.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.04e4f5be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "715c04caf7bf2ac11743",
+    "url": "/static/js/main.26f7a98e.chunk.js"
+  },
+  {
+    "revision": "e055e3fad620f64bc546",
+    "url": "/static/js/runtime-main.d575ac60.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
